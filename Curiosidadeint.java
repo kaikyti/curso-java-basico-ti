56f7f9092737d8572b34bc8114dc8e1cb1c36955
kaikyti <91473984+kaikyti@users.noreply.github.com>
2021-09-27 15:19:49 -0300

    update readme

---
+++ Curiosidadeint.java
@@ -0,0 +1,15 @@
+package com.kaiky.cursojava.aula11;
+
+public class Curiosidadeint {
+
+	public static void main(String[] args) {
+		
+        int var1 = 2147483647;
+		
+		int var2 = 100;
+		
+		System.out.println(var1 + var2);
+
+	}
+
+}
---
+++ Variaveischar.java
@@ -0,0 +1,20 @@
+package com.kaiky.cursojava.aula11;
+
+public class Variaveischar {
+
+	public static void main(String[] args) {
+		
+		//char o = 'o';	
+		//char i = 'i';
+		
+		char o = 111;	
+		char i = 105;
+		char interrogacao = 0x00E1; // valor '?'
+		
+		
+		
+		System.out.println(""+o+i+interrogacao);
+
+	}
+
+}
