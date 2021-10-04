559a5f9d0996e11566b6f5f0e0d5eeaca5856346
kaikyti <91473984+kaikyti@users.noreply.github.com>
2021-10-04 11:09:39 -0300

    update readme

---
+++ Exer13.java
@@ -0,0 +1,32 @@
+package com.kaiky.cursojava.aula13;
+
+import java.util.Scanner;
+
+public class Exer13 {
+
+	public static void main(String[] args) {
+		
+      Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com o valor/hora");
+		double valorHora = scan.nextDouble();
+		
+		System.out.println("Entre com a quantidade de horas trabalhadas no mês:");
+		double horas = scan.nextDouble();
+		
+		double salarioBruto = valorHora * horas;
+		double inss = (salarioBruto / 100) * 8;
+		double sindicato = (salarioBruto / 100) * 5;
+		double ir = (salarioBruto / 100) * 11;
+		double totalDescontos = inss + sindicato + ir;
+		double salarioLiquido = salarioBruto - totalDescontos;
+		
+		System.out.println("Salario bruto: " + salarioBruto);
+		System.out.println("INSS: " + inss);
+		System.out.println("Sindicato: " + sindicato);
+		System.out.println("IR: " + ir);
+		System.out.println("Total descontos: " + totalDescontos); 
+		System.out.println("Salario liquido: " + salarioLiquido);
+	}
+
+}
---
+++ Exer14.java
@@ -0,0 +1,23 @@
+package com.kaiky.cursojava.aula13;
+
+import java.util.Scanner;
+
+public class Exer14 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com o tamanho do arquivo");
+		double tamArquivo = scan.nextDouble();
+		
+		System.out.println("Entre com a velocidade da internet");
+		double velInternet = scan.nextDouble();
+		
+		double tempo = tamArquivo / velInternet;
+		
+		System.out.println("Tempo de download:" + tempo);
+
+	}
+
+}
