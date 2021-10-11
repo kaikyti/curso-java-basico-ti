15e28b0543b5c4af523e07cc2ffe44a350e76fed
kaikyti <91473984+kaikyti@users.noreply.github.com>
2021-10-11 11:27:06 -0300

    update readme

---
+++ LoopWhile.java
@@ -0,0 +1,30 @@
+package com.kaiky.cursojava.aula16;
+
+public class LoopWhile {
+
+	public static void main(String[] args) {
+		
+		
+		int i = 1;
+		int max = 10;
+		
+		System.out.println("Contando até " + max );
+		
+		while (i <= max) {
+			System.out.println("Valor de i: " + i);
+			i++;
+		}
+		
+		
+		System.out.println(i); //valor de 11
+		
+		do {
+			i++;
+			System.out.println("Valor de i: " + i);
+		} while (i < 15);
+		
+		System.out.println(i);
+		
+	}
+
+}
